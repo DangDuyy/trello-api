d67932760eaafd6dfde60669c72d8e125545d5e3
DangDuyy <duyprovn987@gmail.com>
2025-06-27 09:51:54 +0700

    thiết lập env.BUILD_MODE developer và production

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -9,12 +9,6 @@ import ApiError from '~/utils/ApiError'
 
 const createNew = ( async (req, res, next) => {
   try {
-    console.log('req.body ', req.body)
-    console.log('req.query ', req.query)
-    console.log('req.params ', req.params)
-    console.log('req.files ', req.files)
-    console.log('req.cookies ', req.cookies)
-    console.log('req.jwtDecoded ', req.jwtDecoded)
     //dieu huong du lieu sang tang service
 
     // throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Bug!!!')
